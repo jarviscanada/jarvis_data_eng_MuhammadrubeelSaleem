c61af83ff067a1f51f6cfd57c179f166f3a1d5ab
Rubeel Saleem <irubeel@gmail.com>
2025-10-27 07:43:45 +0000

    Add DDL script to create host_info and host_usage tables

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,25 @@
+\c host_agent;
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
+id SERIAL PRIMARY KEY,
+hostname VARCHAR NOT NULL UNIQUE,
+cpu_number INT2 NOT NULL,
+cpu_architecture VARCHAR NOT NULL,
+cpu_model VARCHAR NOT NULL,
+cpu_mhz FLOAT8 NOT NULL,
+l2_cache INT4 NOT NULL,
+total_mem INT4 NOT NULL,
+"timestamp" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
+id SERIAL PRIMARY KEY,
+hostname VARCHAR NOT NULL UNIQUE,
+cpu_number INT2 NOT NULL,
+cpu_architecture VARCHAR NOT NULL,
+cpu_model VARCHAR NOT NULL,
+cpu_mhz FLOAT8 NOT NULL,
+l2_cache INT4 NOT NULL,
+total_mem INT4 NOT NULL,
+"timestamp" TIMESTAMP NOT NULL
+);
